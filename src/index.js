ebddb1f9e61081593af9b36dba4f0a4186b53aa6
mat <mat@MacBook-Air-de-mat.local>
2022-04-19 12:08:52 +0700

    mcd and setup

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import App from './components/App/App.jsx';
 import reportWebVitals from './reportWebVitals';
+import './styles/reset.css';
+import './styles/index.scss';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
